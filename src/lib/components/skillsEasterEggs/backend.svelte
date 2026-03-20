c76f52c7abf0956b1622a65a982ab463387df700
Johan JNN <johan.janin@gmail.com>
2026-03-20 11:17:20 +0100

    additionnal things

--- src/lib/components/skillsEasterEggs/backend.svelte
+++ src/lib/components/skillsEasterEggs/backend.svelte
@@ -7,8 +7,18 @@
       location.origin
     )
   );
+  let updating = $state(false);
+  const TRANSITION_DURATION = 1000;
 
-  $effect(() => console.log(url));
+  function update_img() {
+    updating = true;
+    setTimeout(() => {
+      parameters = { _: Date.now().toString() };
+    }, TRANSITION_DURATION / 2);
+    setTimeout(() => {
+      updating = false;
+    }, TRANSITION_DURATION);
+  }
 </script>
 
 <div class="grid place-content-center justify-items-center gap-4">
@@ -17,6 +27,7 @@
       <img
         src={url.toString()}
         alt="My logo with random colors"
+        class:updating
         class="w-full h-full object-contain"
       />
     {/key}
@@ -26,10 +37,23 @@
     <button
       type="button"
       class="bg-white py-2 px-4 rounded-xl font-medium cursor-pointer hover:rounded-2xl transition-[border-radius]"
-      onclick={() => {
-        console.log("Updating...");
-        parameters = { _: Date.now().toString() };
-      }}>Randomize</button
+      onclick={update_img}
     >
+      Randomize
+    </button>
   </form>
 </div>
+
+<style>
+  @keyframes rotate {
+    from {
+      rotate: 0deg;
+    }
+    to {
+      rotate: 360deg;
+    }
+  }
+  .updating {
+    animation: rotate infinite linear 20ms;
+  }
+</style>
