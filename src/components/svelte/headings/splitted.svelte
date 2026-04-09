81c2c5a7fbc60b564a973997fb14c732c9144f79
Johan JNN <johan.janin@gmail.com>
2026-04-09 12:54:50 +0200

    fix: heading generation issues

--- src/components/svelte/headings/splitted.svelte
+++ src/components/svelte/headings/splitted.svelte
@@ -1,28 +1,42 @@
 <script lang="ts">
+  import type { ClassValue } from "svelte/elements";
   import Heading from "./tag.svelte";
 
   const {
     parts,
     h,
+    class: className,
   }: {
     parts: string[];
     h?: number;
+    class?: ClassValue;
+    unstyled?: boolean;
   } = $props();
+
+  const class_object = $derived(
+    className instanceof Array
+      ? { [className.join(" ")]: true }
+      : typeof className === "string"
+        ? { [className]: true }
+        : className,
+  );
 </script>
 
-<Heading {h}>
-  <span
-    class="text-[10svw] max-sm:text-center sm:text-[6svw]/25 tracking-tighter font-heading uppercase font-bold"
-  >
-    {#each parts as part, index}
-      <span
-        class={{
-          block: true,
-          "text-primary dark:text-secondary tracking-tight": index % 2,
-        }}
-      >
-        {part}
-      </span>
-    {/each}
-  </span>
+<Heading
+  {h}
+  class={{
+    "text-[10svw] max-sm:text-center sm:text-[6svw]/25 tracking-tighter font-heading uppercase font-bold": true,
+    ...class_object,
+  }}
+>
+  {#each parts as part, index}
+    <span
+      class={{
+        block: true,
+        "text-primary dark:text-secondary tracking-tight": index % 2,
+      }}
+    >
+      {part}
+    </span>
+  {/each}
 </Heading>
--- src/components/svelte/headings/tag.svelte
+++ src/components/svelte/headings/tag.svelte
@@ -1,16 +1,19 @@
 <script lang="ts" module>
   import type { Snippet } from "svelte";
+  import type { ClassValue } from "svelte/elements";
 
   export interface HeadingProps {
     children: Snippet;
+    class?: ClassValue | null;
     h?: number;
   }
 </script>
 
 <script lang="ts">
-  const { h = 2, children }: HeadingProps = $props();
+  const { h = 2, children, class: className }: HeadingProps = $props();
+  const tag = $derived(`h${h}`);
 </script>
 
-{@html `<h${h}>`}
-{@render children()}
-{@html `</h${h}>`}
+<svelte:element this={tag} class={className}>
+  {@render children()}
+</svelte:element>
