dbe0ae2aac53b6cbbf3a2e243daead3c59e50c98
Johan JNN <johan.janin@gmail.com>
2026-07-09 16:16:26 +0200

    feat: .pages folder to build the .pages.yml file

---
+++ .pages/build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# ? See https://github.com/rafamel/yaml-import
+
+if [ ! $(which bun) ];
+then
+  echo "::error:: Bun not installed, but required to use the .pages.yml builder (npm not supported yet)"
+  exit 1
+fi
+
+DIRNAME=$(dirname $0)
+FILENAME=".pages.built.yml"
+
+if [ -n "$1" ];
+then
+  FILENAME=$1
+fi
+
+bunx --bun --silent -p yaml-import yimp $DIRNAME/.pages.yml -o $DIRNAME/../$FILENAME
